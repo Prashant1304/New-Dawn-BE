f5de6c7bc04ec9265aeaf2023b073cf0a4de0317
Prashant Sinha <prashant.sinha1@pw.live>
2024-04-26 03:52:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const port = 4000
+const cors = require("cors")
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.use(cors())
+
+app.get("/", cors(), async (req, res)=> {
+    res.send("hello world")
+})
+
+app.listen(port, ()=>{
+console.log(`port is on at ${port}`);
+})
